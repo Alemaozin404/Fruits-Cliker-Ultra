da37e760bc95d4731c0fd5aa1eb554cf86c84696
Willzin__zz <ytdacasa001@gmail.com>
2026-06-04 03:13:03 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='maca-v13-1-new-entry-functional';
-const VERSION='13.1.0-new-entry-functional';
+const CACHE='maca-v13-5-stable-rollback-no-loader';
+const VERSION='13.5.0-stable-rollback-no-loader';
 const ASSETS=['./','./index.html?v='+VERSION,'./style.css?v='+VERSION,'./script.js?v='+VERSION,'./global-config.js?v='+VERSION,'./manifest.webmanifest?v='+VERSION,'./admin/','./admin/index.html?v='+VERSION,'./admin/admin.css?v='+VERSION,'./admin/admin.js?v='+VERSION];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
